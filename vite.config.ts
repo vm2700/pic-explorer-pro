e45d5066896ce9394906d37c46abe3f0057c6ee3
vm2700 <136029877+vm2700@users.noreply.github.com>
2025-09-27 18:23:50 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/pic-explorer-pro/',
+  base: './pic-explorer-pro/',
   server: {
     host: "::",
     port: 8080,
